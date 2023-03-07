b3186ec4731dfaba416b247d90b4b6b543a4df6f
atomdestructo@gmail.com <atomdestructo@gmail.com>
2023-03-07 10:35:45 -0700

    new files

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Pages } from "./pages/Route.js";
+import ContactUs  from "./pages/Components/Sections/ContactUs.js";
+import Copyright  from "./pages/Components/Sections/Copyright.js";
+import Links  from "./pages/Components/Sections/Links.js";
+import LandAck from './pages/Components/Sections/LandAcknowledge.js';
+import Navbar from './pages/Components/Sections/Navbar.js';
+import "./JS/jquery-3.6.3.js";
+import "./Styles/bootstrap.css";
+import "./Styles/main.css";
+import "./Styles/w3.css";
+
+function App() {
+    return (
+        <>
+            <div className="App">
+                <Navbar/>
+                <Pages/>
+                <ContactUs/>
+                <hr/>
+                <br />
+                <Links/>
+                <br/>
+                <hr/>
+                <LandAck/>
+                <hr/>
+                <br/>
+                <Copyright/>
+            </div>
+        </>
+        );
+    }
+        export default App;
